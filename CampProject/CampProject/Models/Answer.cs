6d77bec91463ab0eda69d179626384e2ffc238a5
Andriana <khariv.a.r@gmail.com>
2022-09-16 21:48:06 +0300

    fixed project issue

---
+++ CampProject/CampProject/Models/Answer.cs
@@ -0,0 +1,12 @@
+﻿namespace CampProject.Models
+{
+    public class Answer
+    {
+        public int id { get; set; } 
+        public string? Text { get; set; }    
+
+        public int QuestionId { get; set; } 
+        public virtual Question Question { get; set; }  
+
+    }
+}
---
+++ CampProject/CampProject/Models/Statistics.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CampProject.Models
+{
+    [Keyless]
+    public class Statistics
+    {
+        public int User_id { get; set; }
+        public int QuizId { get; set; }
+        public virtual Quiz Quiz { get; set; }
+
+    }
+}
